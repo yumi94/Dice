acee62938ad5945d2f9a18ddf6bddcddfb5b1620
Yumi <spr1238@gmail.com>
2023-04-27 16:11:10 +0900

    first Commit

---
+++ src/test/java/my/DiceGame/DiceGameApplicationTests.java
@@ -0,0 +1,13 @@
+package my.DiceGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiceGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
